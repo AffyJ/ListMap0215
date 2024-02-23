95d0ef2d17d13d091dd39c554ac918b2471d5782
progra33erJ <3affy.happy7@gmail.com>
2024-02-23 11:29:39 +0900

    例外処理の修正。ワイルドカードの無効化。

--- src/main/java/com/jully/Main.java
+++ src/main/java/com/jully/Main.java
@@ -1,11 +1,15 @@
 package com.jully;
 
-import java.util.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         List<String> names = new ArrayList<>();
         names.add("Miley Cyrus");
         names.add("Taylor Swift");
@@ -22,42 +26,22 @@ public class Main {
         System.out.println(names.get(4));
 
 
-
         Map<String, String> profileMap = new HashMap<>();
-        profileMap.put("Miley Cyrus", "Record of the Year & Best Pop Solo Performance" );
-        profileMap.put("Taylor Swift","Album of the Year & Best Pop Vocal Album" );
+        profileMap.put("Miley Cyrus", "Record of the Year & Best Pop Solo Performance");
+        profileMap.put("Taylor Swift", "Album of the Year & Best Pop Vocal Album");
         profileMap.put("Billie Eilish", "Song for the Year");
-        profileMap.put("Victoria Monét","Best New Artist" );
-        profileMap.put("The Beatles","Best Music Video" );
-
-
-
-        if(profileMap.containsKey("Miley Cyrus")){
-            System.out.println("Miley Cyrus is a winner.");
-            System.out.println("She won " + profileMap.get("Miley Cyrus") + ".");
-        }else{
-            System.out.println("Sorry. No information founded.");
-        }
-
-
+        profileMap.put("Victoria Monét", "Best New Artist");
+        profileMap.put("The Beatles", "Best Music Video");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please Enter artist's name:");
-        String name = scanner.nextLine();
-        if (profileMap.containsKey(name)){
-            System.out.println("This artist won " + profileMap.get(name) + ".");
-        }else{
-            System.out.println("Sorry. No information founded.");
+
+        System.out.print("Please enter the artist's name: ");
+        String artistName = scanner.nextLine();
+
+        if (profileMap.get(artistName) != null) {
+            System.out.println("This artist won " + profileMap.get(artistName));
+        } else {
+            System.out.println("Sorry. No information found.");
         }
-
-
-
-
-
-
-
-
-
-
     }
 }
